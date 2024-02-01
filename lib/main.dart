339091fa57e099ad4310280022101ae4ccaa4d21
Panzershack <pasannim@yahoo.com>
2024-02-01 19:00:03 +0530

    enlarged buttons for a bettwe view, changed the styles to look like flatButtons

--- lib/main.dart
+++ lib/main.dart
@@ -14,81 +14,121 @@ class XylophoneApp extends StatelessWidget {
     await player.play(AssetSource(sound));
   }
 
+  final ButtonStyle flatButtonStyle = TextButton.styleFrom(
+    primary: Colors.black87,
+    minimumSize: Size(88, 36),
+    padding: EdgeInsets.symmetric(horizontal: 16),
+    shape: const RoundedRectangleBorder(
+      borderRadius: BorderRadius.all(Radius.circular(2)),
+    ),
+  );
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: SafeArea(
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
-              TextButton(
-                onPressed: () {
-                  playSound('note1.wav');
-                },
-                child: Container(
-                  width: 100.0,
-                  height: 40.0,
-                  color: Colors.red,
+              Expanded(
+                child: Padding(
+                  padding: EdgeInsets.all(0.0),
+                  child: TextButton(
+                    style: flatButtonStyle,
+                    onPressed: () {
+                      playSound('note1.wav');
+                    },
+                    child: Container(
+                      margin: EdgeInsets.zero,
+                      color: Colors.red,
+                    ),
+                  ),
                 ),
               ),
-              TextButton(
-                onPressed: () {
-                  playSound('note2.wav');
-                },
-                child: Container(
-                  width: 100.0,
-                  height: 40.0,
-                  color: Colors.orange,
+              Expanded(
+                child: Padding(
+                  padding: EdgeInsets.all(0.0),
+                  child: TextButton(
+                    style: flatButtonStyle,
+                    onPressed: () {
+                      playSound('note2.wav');
+                    },
+                    child: Container(
+                      color: Colors.orange,
+                    ),
+                  ),
                 ),
               ),
-              TextButton(
-                onPressed: () {
-                  playSound('note3.wav');
-                },
-                child: Container(
-                  width: 100.0,
-                  height: 40.0,
-                  color: Colors.yellow,
+              Expanded(
+                child: Padding(
+                  padding: EdgeInsets.all(0.0),
+                  child: TextButton(
+                    style: flatButtonStyle,
+                    onPressed: () {
+                      playSound('note3.wav');
+                    },
+                    child: Container(
+                      color: Colors.yellow,
+                    ),
+                  ),
                 ),
               ),
-              TextButton(
-                onPressed: () {
-                  playSound('note4.wav');
-                },
-                child: Container(
-                  width: 100.0,
-                  height: 40.0,
-                  color: Colors.green,
+              Expanded(
+                child: Padding(
+                  padding: EdgeInsets.all(0.0),
+                  child: TextButton(
+                    style: flatButtonStyle,
+                    onPressed: () {
+                      playSound('note4.wav');
+                    },
+                    child: Container(
+                      color: Colors.green,
+                    ),
+                  ),
                 ),
               ),
-              TextButton(
-                onPressed: () {
-                  playSound('note5.wav');
-                },
-                child: Container(
-                  width: 100.0,
-                  height: 40.0,
-                  color: Colors.blue,
+              Expanded(
+                child: Padding(
+                  padding: EdgeInsets.all(0.0),
+                  child: TextButton(
+                    style: flatButtonStyle,
+                    onPressed: () {
+                      playSound('note5.wav');
+                    },
+                    child: Container(
+                      color: Colors.blue,
+                    ),
+                  ),
                 ),
               ),
-              TextButton(
-                onPressed: () {
-                  playSound('note6.wav');
-                },
-                child: Container(
-                  width: 100.0,
-                  height: 40.0,
-                  color: Colors.indigo,
+              Expanded(
+                child: Padding(
+                  padding: EdgeInsets.all(0.0),
+                  child: TextButton(
+                    style: flatButtonStyle,
+                    onPressed: () {
+                      playSound('note6.wav');
+                    },
+                    child: Container(
+                      color: Colors.indigo,
+                    ),
+                  ),
                 ),
               ),
-              TextButton(
-                onPressed: () {
-                  playSound('note7.wav');
-                },
-                child: Container(
-                  width: 100.0,
-                  height: 40.0,
-                  color: Colors.purple,
+              Expanded(
+                child: Padding(
+                  padding: EdgeInsets.all(0.0),
+                  child: TextButton(
+                    style: flatButtonStyle,
+                    onPressed: () {
+                      playSound('note7.wav');
+                    },
+                    child: Container(
+                      color: Colors.purple,
+                    ),
+                  ),
                 ),
               ),
             ],
